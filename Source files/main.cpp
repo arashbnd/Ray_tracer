4399f919eed3c74a8ce8f84669c163a950c19d5e
Arash bahramian <arash.bahramiand@gmail.com>
2023-11-28 10:06:07 -0800

    created window using imgui

--- Source files/main.cpp
+++ Source files/main.cpp
@@ -32,34 +32,74 @@ Projectile tick(Environment& env, Projectile& proj);
     }
 
     int main() {
+        // GLFW initialization
+        if (!glfwInit()) {
+            std::cerr << "Failed to initialize GLFW" << std::endl;
+            return -1;
+        }
 
-        glfwInit(); // Initialize GLFW
-
-        // Create a window and OpenGL context
-        GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Example", nullptr, nullptr);
-        if (window == nullptr) {
+        // Create a GLFW window
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        GLFWwindow* window = glfwCreateWindow(800, 600, "ImGui GLFW OpenGL3 Example", nullptr, nullptr);
+        if (!window) {
+            std::cerr << "Failed to create GLFW window" << std::endl;
             glfwTerminate();
             return -1;
         }
 
-        glfwMakeContextCurrent(window); // Set the current OpenGL context
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
 
+        // Initialize GLEW (OpenGL Extension Wrangler)
+        if (glewInit() != GLEW_OK) {
+            std::cerr << "Failed to initialize GLEW" << std::endl;
+            glfwTerminate();
+            return -1;
+        }
+
+        // Setup Dear ImGui context
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
-        ImGui::StyleColorsDark();
+        ImGuiIO& io = ImGui::GetIO();
+
+        // Initialize ImGui for GLFW and OpenGL3
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init("#version 330");
 
+        // Main loop
         while (!glfwWindowShouldClose(window)) {
-            display(); // Call the display function
+            glfwPollEvents();
 
-            glfwSwapBuffers(window); // Swap buffers
-            glfwPollEvents(); // Process events
+            // Start the ImGui frame
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            // Create your ImGui GUI here
+            ImGui::Begin("Hello, ImGui!");
+            ImGui::Text("This is a basic ImGui window.");
+            if (ImGui::Button("Exit")) {
+                glfwSetWindowShouldClose(window, true);
+            }
+            ImGui::End();
+
+            // Rendering
+            glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            glfwSwapBuffers(window);
         }
 
-        glfwTerminate(); // Clean up and terminate GLFW
+        // Cleanup
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        glfwTerminate();
 
+        
 	return 0;
 
 }
